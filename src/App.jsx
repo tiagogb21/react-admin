d66c04eb7690b6ea4345aeb99b7fef982800f5ae
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-30 14:00:06 -0300

    configurando pagina inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
